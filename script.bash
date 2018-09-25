4bf9ab3478eff4278156c2abe7f5da71e33d6fdf
V1P3R-NON <43217047+V1P3R-NON@users.noreply.github.com>
2018-09-25 21:52:53 +0700

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-FILES="./"
+A="A_(\d{8})_(\d{4}).*.log"
+B="B_(\d{12}).*.log"
+C="C_(\d{4})_(\d{2})_(\d{2}).*.log"
+
+#Config Path
+pathlogfile=".............."
+targat=".............."
+
+mkdir rawfile
+mv $(find $pathlogfile -name "*"|grep -P "$A|$B|$C"| sed "s|^\./||") rawfile/
+
+FILES="rawfile/"
 
 for f in "$FILES"*.log
 do
 
-        temp=${f//_/}
-        temp=${temp:2:-10}
+temp=${f//_/}
+temp=${temp:7:-10}
 
-        dir1=${temp: -6:-2}
-        dir2=${temp: -2:2}
-        dir3=${temp//[0-9]/}
+echo $temp
 
-        dir=$dir1/$dir2/$dir3
+dir1=${temp: -6:-2}
+dir2=${temp: -2:2}
+dir3=${temp//[0-9]/}
 
-        if [[ ! -e $dir ]]
-        then
-                mkdir -p $dir
-        fi
+dir=$dir1/$dir2/$dir3
+echo $dir
+if [[ ! -e $dir ]]
+then
+mkdir -p $targat/$dir
+fi
 
-        mv $f $dir
+mv $f $targat/$dir
+
 done
+rm -rf rawfile
